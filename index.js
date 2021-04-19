78b6404bb6984f9a14299a5bf96226d06762170a
Egregious <dvdscheibel@yahoo.com>
2021-04-19 15:59:36 -0400

    we're setting it up

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+BASE_URL = "http://localhost:3000/farms/"
 
+fetch(BASE_URL)
+    .then (res => res.json())
+    .then (data => console.log(data))
+//         .then (data => data.forEach((farms) => {
+//             renderFarms(farms)
+//     }))
+
+// function renderFarms(farms) {
+//     const farmsSpan = document.createElement("span")
+//           farmsSpan.id = farms.id
+
+//     const farmName = document.createElement("h3")
+//           farmName.innerText = farms.name
+    
+//     farmsSpan.append(farmName)
+
+//     document.getElementById("farm-bar").append(farmsSpan)
+// }
