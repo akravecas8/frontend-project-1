ffac4df25cf39170c0184337975e5858b9f75d93
Egregious <dvdscheibel@yahoo.com>
2021-04-20 16:02:50 -0400

    frontend update v3

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ fetch(BASE_URL)
     // .then (data => console.log(data))
     .then (data => data.forEach((farms) => {
         renderFarms(farms)
-        renderProduce(farms.produce)
     }))
 
 // function renderFarms(farms) {
@@ -28,34 +27,84 @@ fetch(BASE_URL)
 
 function renderFarms(farms) {
 
+    const farmSpan = document.createElement("span")
+          farmSpan.id = farms.id
+
     const farmName = document.createElement("h3")
           farmName.innerText = farms.name
-          farmName.addEventListener("click", (e) => {
-              e.preventDefault()
-          })
-    
+          
     const farmImg = document.createElement("image")
           farmImg.src = farms.image
-
-    document.getElementById("spanny").append(farmName, farmImg)
+                
+    farmSpan.append(farmName, farmImg)
+            
+    document.getElementById("navvy").append(farmSpan)
+                
+    farmSpan.addEventListener("click", () => {
+            renderProduce(farms)
+        })     
 }
 
-function renderProduce(produce) {
+function renderProduce(farms) {
+    console.log(farms)
 
-    const produceName = document.createElement("h2")
-          produceName.innerText = produce.produceName
-    const produceAvail = document.createElement("h3")
-          produceAvail
-            if (produce.available === true) {
-                produceAvail.innerText = "In Stock"
-            }
-            else "Out of Stock"
-    const produceQuant = document.createElement("h3")
-          produceQuant.innerText = produce.quantity
-    const producePrice = document.createElement("h3")
-          producePrice.innerText = produce.cost
-    // const produceImage = document.createElement("image")
-    //       produceImage.src = produce.produceImage
+    for (i=0; i < farms.produce.length; i++) {
+
+        const itemCard = document.createElement("div")
+              itemCard.id = i
+
+        const produceName = document.createElement("h2")
+              produceName.innerText = farms.produce[i].produceName
+
+        const produceAvail = document.createElement("h3")
+              produceAvail
+                if (farms.produce[i].available === true) {
+                    produceAvail.innerText = "In Stock"
+                    }
+                else produceAvail.innerText = "Out of Stock"
+
+        const produceQuant = document.createElement("h3")
+              produceQuant.innerText = farms.produce[i].quantity
+
+        const producePrice = document.createElement("h3")
+              producePrice.innerText = farms.produce[i].cost
+
+        const produceImage = document.createElement("image")
+              produceImage.src = produce.produceImage
+
+        const addToCartButton = document.createElement("button")
+              addToCartButton.innerText = "Add to Cart"
+              addToCartButton.id = farms.produce[i].produceName
+              addToCartButton.class = farms.produce[i].cost
+              addToCartButton.name = farms.produce[i].produceImage
+              addToCartButton.addEventListener("click", (e) =>{
+                    fillCart(e)
+              })
+
+        itemCard.append(produceName, produceAvail, produceQuant, producePrice, produceImage, addToCartButton)    
+
+        document.getElementById("produce").append(itemCard)
+        }
+}
+
+function fillCart(e) {
+    console.log(e)
+    // debugger
+
+    let i = 0
     
-    document.getElementById("produce").append(produceName, produceAvail, produceQuant, producePrice)
+    const cartCard = document.createElement("div")
+
+    const produceName = document.createElement("h3")
+          produceName.innerText = e.target.id
+    let produceQuant = document.createElement("h3")
+          produceQuant = i++
+    let produceCost = document.createElement("h3")
+          produceCost = e.target.class * produceQuant
+    const produceImg = document.createElement("image")
+          produceImg.src = e.target.name
+
+    cartCard.append(produceName, produceQuant, produceCost, produceImg)
+
+    document.getElementById("shopping-cart").append(cartCard)
 }
