2bce15b3ccdb93241d470bb32c35ee556589ed6b
Egregious <dvdscheibel@yahoo.com>
2021-04-21 09:14:40 -0400

    update frontend v4

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-BASE_URL = "http://localhost:3000/farms/"
+FARMS_URL = "http://localhost:3000/farms/"
+CART_URL = "http://localhost:3000/cart/"
 
-fetch(BASE_URL)
+const checkoutCart = [{}]
+
+fetch(FARMS_URL)
     .then (res => res.json())
     // .then (data => console.log(data))
     .then (data => data.forEach((farms) => {
         renderFarms(farms)
     }))
-
-// function renderFarms(farms) {
-//     const farmsSpan = document.createElement("span")
-//           farmsSpan.id = farms.id
-//           farmsSpan.addEventListener("click", (e) =>{
-//                 e.preventDefault()
-//           })
-
-//     const farmName = document.createElement("h3")
-//           farmName.innerText = farms.name
-    
-//     const farmImg = document.createElement("image")
-//           farmImg.src = farms.image
-    
-//     farmsSpan.append(farmName, farmImg)
-
-//     document.getElementById("farm-bar").append(farmsSpan)
-// }
 
 function renderFarms(farms) {
 
@@ -42,7 +27,7 @@ function renderFarms(farms) {
                 
     farmSpan.addEventListener("click", () => {
             renderProduce(farms)
-        })     
+        })
 }
 
 function renderProduce(farms) {
@@ -88,23 +73,60 @@ function renderProduce(farms) {
 }
 
 function fillCart(e) {
-    console.log(e)
-    // debugger
-
-    let i = 0
     
-    const cartCard = document.createElement("div")
+    const cartObj = {"name":e.target.id, "cost":e.target.class, "qty":1}
+    const qty = cartObj.qty
+    const newQty = qty + 1
 
-    const produceName = document.createElement("h3")
-          produceName.innerText = e.target.id
-    let produceQuant = document.createElement("h3")
-          produceQuant = i++
-    let produceCost = document.createElement("h3")
-          produceCost = e.target.class * produceQuant
-    const produceImg = document.createElement("image")
-          produceImg.src = e.target.name
+    if (checkoutCart.indexOf("name") !== e.target.id)   
+       (checkoutCart.push(cartObj))
+    else iterateCart()
 
-    cartCard.append(produceName, produceQuant, produceCost, produceImg)
+    function iterateCart() {
+        for (i=0; i<checkoutCart.length; i++) {
+            if (checkoutCart.name === e.target.id)
+                checkoutCart.qty = newQty
+            else console.log("I didn't increment qty")
+        }  
+    }
 
-    document.getElementById("shopping-cart").append(cartCard)
+         //maybe pass in a function to the 'else' argument that can handle more rigorous
+         //logic than the 'else' is capable of?
+
+    // for (i=0; i<checkoutCart.length; i++) {
+    //     if (checkoutCart.name !== e.target.id)   
+    //        (checkoutCart.push(cartObj))
+    //     else (+checkoutCart.qty + 1)
+    //     }
+    
+    // if (checkoutCart.name !== e.target.id)   
+    //    (checkoutCart.push(cartObj))
+    // else (checkoutCart.qty + 1)
+
+    // if (checkoutCart[1] === e.target.id)   
+    //    ((checkoutCart[3]) + 1)
+    // else (checkoutCart.push(cartObj))
 }
+
+
+
+// fetch(CART_URL) 
+//     .then (res => res.json())
+//     .then (data => console.log(data))
+
+// let i = 0
+
+// const cartCard = document.createElement("div")
+
+// const produceName = document.createElement("h3")
+//       produceName.innerText = e.target.id
+// const produceQuant = document.createElement("h4")
+//       produceQuant.innerText = i++
+// const produceCost = document.createElement("h4")
+//       produceCost.innerText = e.target.class * produceQuant
+// const produceImg = document.createElement("image")
+//       produceImg.src = e.target.name
+
+// cartCard.append(produceName, produceQuant, produceCost, produceImg)
+
+// document.getElementById("shopping-cart").append(cartCard)
