22147fd9345d9f815f5613bdbab7f5d96ebcf1ae
Egregious <dvdscheibel@yahoo.com>
2021-04-21 15:39:00 -0400

    test shit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ fetch(FARMS_URL)
     // .then (data => console.log(data))
     .then (data => data.forEach((farms) => {
         renderFarms(farms)
+        console.log(farms.produce)
     }))
 
 function renderFarms(farms) {
@@ -24,18 +25,24 @@ function renderFarms(farms) {
     farmSpan.append(farmName, farmImg)
             
     document.getElementById("navvy").append(farmSpan)
-                
+    
+    document.getElementById("add-produce-form").addEventListener("submit", (e) => addProduceItem(e, farms))
+          
     farmSpan.addEventListener("click", () => {
             renderProduce(farms)
-        })
+
+            const createBtnId = document.querySelector(".create-item-button")
+                  createBtnId.id = farms.id
+    })  
 }
 
 function renderProduce(farms) {
     console.log(farms)
-
+    
     for (i=0; i < farms.produce.length; i++) {
 
         const itemCard = document.createElement("div")
+              itemCard.className = "box"
               itemCard.id = i
 
         const produceName = document.createElement("h2")
@@ -66,10 +73,56 @@ function renderProduce(farms) {
                     fillCart(e)
               })
 
-        itemCard.append(produceName, produceAvail, produceQuant, producePrice, produceImage, addToCartButton)    
+        const updateButton = document.createElement("button")
+              updateButton.innerText = "Update Item"
+              updateButton.id = itemCard.id
+              updateButton.addEventListener("click", (e) =>{
+                    e.preventDefault()
+              })
+
+        const deleteButton = document.createElement("button")
+              deleteButton.innerText = "Delete Item"
+              deleteButton.id = itemCard.id
+              deleteButton.addEventListener("click", (e) =>{
+                    e.preventDefault()
+              })
+
+        itemCard.append(produceName, produceAvail, produceQuant, producePrice, produceImage, addToCartButton, updateButton, deleteButton)    
 
         document.getElementById("produce").append(itemCard)
+        // debugger
         }
+}
+
+function addProduceItem(e, farms) {
+    // debugger
+        // fetch(FARMS_URL)
+        //     .then (res => res.json())
+        //     .then (data => data.forEach(data))
+
+        const addNewProduce = {produce: {
+            produceName: e.target[0].value,
+            available: e.target[1].value,
+            quantity: e.target[2].value,
+            cost: e.target[3].value,
+            produceImage: "placeholder",
+        }}
+
+        const spanNewProduce = {
+            produce: [... farms.produce, addNewProduce]
+        }
+            
+        const newObj = {
+            headers: {"Content-Type": "application/json"},
+            method: "PATCH",
+            body: JSON.stringify(spanNewProduce),
+        }
+
+        fetch(FARMS_URL+e.target[4].id, newObj)
+            .then(res => res.json())
+            .then(renderProduce)
+    
+        e.target.reset()
 }
 
 function fillCart(e) {
